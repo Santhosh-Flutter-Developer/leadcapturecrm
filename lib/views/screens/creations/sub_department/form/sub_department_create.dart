adacdcb3bc572cddeb1ffb05cd26495e10949023
Srisoftwarez <125208819+srisoftwarez-projects@users.noreply.github.com>
2026-02-18 12:25:37 +0530

    add sub department issue fixed

--- lib/views/screens/creations/sub_department/form/sub_department_create.dart
+++ lib/views/screens/creations/sub_department/form/sub_department_create.dart
@@ -133,7 +133,7 @@ class _SubDepartmentCreateState extends State<SubDepartmentCreate> {
               label: 'Department',
               isRequired: true,
               validator: (value) {
-                if (value == null) {
+                if (_selectedDepartment == null) {
                   return 'Department is required';
                 }
                 return null;
@@ -147,6 +147,7 @@ class _SubDepartmentCreateState extends State<SubDepartmentCreate> {
                 if (selectedDep != null) {
                   _selectedDepartment = selectedDep;
                 }
+                print("the dep $_selectedDepartment ,,, $selectedDep");
                 setState(() {});
               },
             ),
